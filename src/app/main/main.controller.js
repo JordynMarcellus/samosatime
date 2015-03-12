7346a273e17fc506946c70007d30abb14160e90f
ruhee.dewji <ruhee.dewji@gmail.com>
2015-03-11 20:48:50 -0400

    samosa list and some loops

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,5 +1,15 @@
 'use strict';
 
 angular.module('samosatime')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('data/samosas.json').success(function(data){
+      var samosas = data.samosas;
+      var output = '<ul>';
+      for(var i = 0; i < samosas.length; i++){
+        output += '<li>' + samosas[i].name + '</li>';
+      }
+      output += '</ul>';
+
+      console.log(output);
+    })
   });
