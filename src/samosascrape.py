3bfaca4fa46542fdddc3fb6a70b038c00d404ec7
ruhee.dewji <ruhee.dewji@gmail.com>
2015-05-07 21:04:41 -0400

    Whoa dog a scraper!

--- src/samosascrape.py
+++ src/samosascrape.py
@@ -2,29 +2,27 @@
 import json
 import urllib2
 import requests
+import string
 from bs4 import BeautifulSoup
 
-base_url = 'http://www.yelp.ca'
-url = 'http://www.yelp.ca/search?find_desc=samosa&find_loc=Toronto%2C+ON&ns=1'
-soup = BeautifulSoup(urllib2.urlopen(url).read())
-
 data = []
-pages = []
 
-r = requests.get(url)
-nextlink = soup.find('a', { 'class': 'next' })['href']
-pages.append(nextlink)
+for x in xrange(0,39):
+	url_append = x*10
+	url = 'http://www.yelp.ca/search?find_desc=samosa&find_loc=Toronto%2C+ON&start='+str(url_append)
+	soup = BeautifulSoup(urllib2.urlopen(url).read())
 
-for listing in soup.find_all('div', attrs={'class': 'biz-listing-large'}):
-	data.append({
-		"name": listing.find('a',attrs={'class':'biz-name'}).contents[0],
-		"address": listing.find('address').contents[0].lstrip() # removes newline and leading whitespace
+	for listing in soup.findAll('div', attrs={'class': 'biz-listing-large'}):
+		name = listing.find('a',attrs={'class':'biz-name'}).strings
+		name = "".join(name)
+		address = listing.find('address').contents[0].lstrip() # removes newline and leading whitespace
+		phone = listing.find('span',attrs={'class':'biz-phone'}).contents[0].lstrip().rstrip()
+
+		data.append({
+			"name": "".join(name),
+			"address": address,
+			"phone": phone
 		})
 
-# print data
+#print data
 json.dump(data, open("data/samosadump.json", 'w'), indent=1)
-
-# todo: 
-# - exclude first search result (advertisement)
-# - handle paginated results
-# - add other data (phone? $? etc)
